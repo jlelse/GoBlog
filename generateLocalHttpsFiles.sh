ee02d0e03bd60de7f1a7ef5eccb747d79ad5b02a
Jan-Lukas Else <mail@jlelse.de>
2020-09-24 18:50:38 +0200

    Remove scripts

--- generateLocalHttpsFiles.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-openssl genrsa -out https/server.key 2048
-openssl ecparam -genkey -name secp384r1 -out https/server.key
-openssl req -new -x509 -sha256 -key https/server.key -out https/server.crt -days 3650
