1218def2b6b80f8a33e6a1c1ae3d566e8fa2f2ba
Jan-Lukas Else <mail@jlelse.de>
2021-03-09 21:35:17 +0100

    Add all public URLs to sitemap

--- sitemap.go
+++ sitemap.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
@@ -11,29 +12,132 @@ import (
 const sitemapPath = "/sitemap.xml"
 
 func serveSitemap(w http.ResponseWriter, r *http.Request) {
-	posts, err := getPosts(&postsRequestConfig{
-		status: statusPublished,
-	})
-	if err != nil {
-		serveError(w, r, err.Error(), http.StatusInternalServerError)
-		return
-	}
 	sm := sitemap.New()
 	sm.Minify = true
-	for _, p := range posts {
-		item := &sitemap.URL{
-			Loc: p.fullURL()}
-		var lastMod time.Time
-		if p.Updated != "" {
-			lastMod, _ = dateparse.ParseLocal(p.Updated)
+	// Blogs
+	for b, bc := range appConfig.Blogs {
+		// Blog
+		blogPath := bc.Path
+		if blogPath == "/" {
+			blogPath = ""
 		}
-		if p.Published != "" && lastMod.IsZero() {
-			lastMod, _ = dateparse.ParseLocal(p.Published)
+		sm.Add(&sitemap.URL{
+			Loc: appConfig.Server.PublicAddress + blogPath,
+		})
+		// Sections
+		for _, section := range bc.Sections {
+			if section.Name != "" {
+				sm.Add(&sitemap.URL{
+					Loc: appConfig.Server.PublicAddress + bc.getRelativePath("/"+section.Name),
+				})
+			}
 		}
-		if !lastMod.IsZero() {
-			item.LastMod = &lastMod
+		// Taxonomies
+		for _, taxonomy := range bc.Taxonomies {
+			if taxonomy.Name != "" {
+				// Taxonomy
+				taxPath := bc.getRelativePath("/" + taxonomy.Name)
+				sm.Add(&sitemap.URL{
+					Loc: appConfig.Server.PublicAddress + taxPath,
+				})
+				// Values
+				if taxValues, err := allTaxonomyValues(b, taxonomy.Name); err == nil {
+					for _, tv := range taxValues {
+						sm.Add(&sitemap.URL{
+							Loc: appConfig.Server.PublicAddress + taxPath + "/" + urlize(tv),
+						})
+					}
+				}
+			}
 		}
-		sm.Add(item)
+		// Year / month archives
+		if dates, err := allPublishedDates(b); err == nil {
+			already := map[string]bool{}
+			for _, d := range dates {
+				// Year
+				yearPath := bc.getRelativePath("/" + fmt.Sprintf("%0004d", d.year))
+				if !already[yearPath] {
+					sm.Add(&sitemap.URL{
+						Loc: appConfig.Server.PublicAddress + yearPath,
+					})
+					already[yearPath] = true
+				}
+				// Specific month
+				monthPath := yearPath + "/" + fmt.Sprintf("%02d", d.month)
+				if !already[monthPath] {
+					sm.Add(&sitemap.URL{
+						Loc: appConfig.Server.PublicAddress + monthPath,
+					})
+					already[monthPath] = true
+				}
+				// Specific day
+				dayPath := monthPath + "/" + fmt.Sprintf("%02d", d.day)
+				if !already[dayPath] {
+					sm.Add(&sitemap.URL{
+						Loc: appConfig.Server.PublicAddress + dayPath,
+					})
+					already[dayPath] = true
+				}
+				// Generic month
+				genericMonthPath := blogPath + "/x/" + fmt.Sprintf("%02d", d.month)
+				if !already[genericMonthPath] {
+					sm.Add(&sitemap.URL{
+						Loc: appConfig.Server.PublicAddress + genericMonthPath,
+					})
+					already[genericMonthPath] = true
+				}
+				// Specific day
+				genericMonthDayPath := genericMonthPath + "/" + fmt.Sprintf("%02d", d.day)
+				if !already[genericMonthDayPath] {
+					sm.Add(&sitemap.URL{
+						Loc: appConfig.Server.PublicAddress + genericMonthDayPath,
+					})
+					already[genericMonthDayPath] = true
+				}
+			}
+		}
+		// Photos
+		if bc.Photos != nil && bc.Photos.Enabled {
+			sm.Add(&sitemap.URL{
+				Loc: appConfig.Server.PublicAddress + bc.getRelativePath(bc.Photos.Path),
+			})
+		}
+		// Search
+		if bc.Search != nil && bc.Search.Enabled {
+			sm.Add(&sitemap.URL{
+				Loc: appConfig.Server.PublicAddress + bc.getRelativePath(bc.Search.Path),
+			})
+		}
+		// Stats
+		if bc.BlogStats != nil && bc.BlogStats.Enabled {
+			sm.Add(&sitemap.URL{
+				Loc: appConfig.Server.PublicAddress + bc.getRelativePath(bc.BlogStats.Path),
+			})
+		}
+		// Custom pages
+		for _, cp := range bc.CustomPages {
+			sm.Add(&sitemap.URL{
+				Loc: appConfig.Server.PublicAddress + cp.Path,
+			})
+		}
 	}
+	// Posts
+	if posts, err := getPosts(&postsRequestConfig{status: statusPublished}); err == nil {
+		for _, p := range posts {
+			item := &sitemap.URL{Loc: p.fullURL()}
+			var lastMod time.Time
+			if p.Updated != "" {
+				lastMod, _ = dateparse.ParseLocal(p.Updated)
+			}
+			if p.Published != "" && lastMod.IsZero() {
+				lastMod, _ = dateparse.ParseLocal(p.Published)
+			}
+			if !lastMod.IsZero() {
+				item.LastMod = &lastMod
+			}
+			sm.Add(item)
+		}
+	}
+	// Write...
 	_, _ = sm.WriteTo(w) // Already minified
 }
