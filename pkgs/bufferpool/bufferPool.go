5268529b1223b263e9c5038c245c1728c5406a3e
Jan-Lukas Else <mail@jlelse.de>
2022-01-26 12:04:20 +0100

    Remove useless mutex

--- pkgs/bufferpool/bufferPool.go
+++ pkgs/bufferpool/bufferPool.go
@@ -11,17 +11,11 @@ var bufferPool = sync.Pool{
 	},
 }
 
-var poolMutex sync.Mutex
-
 func Get() *bytes.Buffer {
-	poolMutex.Lock()
-	defer poolMutex.Unlock()
 	return bufferPool.Get().(*bytes.Buffer)
 }
 
 func Put(bufs ...*bytes.Buffer) {
-	poolMutex.Lock()
-	defer poolMutex.Unlock()
 	for _, buf := range bufs {
 		buf.Reset()
 		bufferPool.Put(buf)
