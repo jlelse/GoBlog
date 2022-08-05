c559cf7ea976c5e703e20994c8354f6410c11b00
Jan-Lukas Else <mail@jlelse.de>
2022-08-05 09:38:47 +0200

    Replace GC ticker with 100 MB memory limit, should be enough

--- garbagecollector.go
+++ garbagecollector.go
@@ -1,29 +1,10 @@
 package main
 
 import (
-	"log"
-	"runtime"
-	"time"
+	"runtime/debug"
 )
 
 func initGC() {
-	go func() {
-		ticker := time.NewTicker(15 * time.Minute)
-		for range ticker.C {
-			doGC()
-		}
-	}()
-}
-
-func doGC() {
-	var before, after runtime.MemStats
-	runtime.ReadMemStats(&before)
-	runtime.GC()
-	runtime.ReadMemStats(&after)
-	log.Printf(
-		"\nAlloc: %d MiB -> %d MiB\nSys: %d MiB -> %d MiB\nNumGC: %d",
-		before.Alloc/1024/1024, after.Alloc/1024/1024,
-		before.Sys/1024/1024, after.Sys/1024/1024,
-		after.NumGC,
-	)
+	// Set memory limit to 100 MB
+	debug.SetMemoryLimit(100 * 1000 * 1000)
 }
